f365e2bd40ceafe584b9ac6439f311cb9cf7bffb
jsonggan <1006283@mymail.sutd.edu.sg>
2024-10-05 01:05:38 +0800

    initialization

---
+++ src/test/java/com/communication/communication_backend/CommunicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communication.communication_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
