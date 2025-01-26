c369f65f1835ac3f9320789df83efd599b430e7f
chin song <1006283@mymail.sutd.edu.sg>
2025-01-26 17:20:28 +0800

    cors update

--- src/main/java/com/communication/communication_backend/config/SecurityConfiguration.java
+++ src/main/java/com/communication/communication_backend/config/SecurityConfiguration.java
@@ -6,14 +6,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfiguration {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .bearerTokenResolver(customBearerTokenResolver()) // Use custom resolver
                         .jwt(Customizer.withDefaults()) // Use default JWT decoding
@@ -24,5 +32,31 @@ public class SecurityConfiguration {
     @Bean
     public BearerTokenResolver customBearerTokenResolver() {
         return new CustomBearerTokenResolver();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // Define allowed origins
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://frontend.itor-sg.com"));
+
+        // Define allowed HTTP methods
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Define allowed headers
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        // Allow credentials
+        configuration.setAllowCredentials(true);
+
+        // Optionally, expose headers
+        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+
+        // Apply CORS configuration to all paths
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
