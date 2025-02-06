fd1661d09facf4892a3be3c57985745c8ec14374
Lin Yixiu <melodicoder@users.noreply.github.com>
2025-02-07 01:32:37 +0800

    Add files via upload

---
+++ src/test/java/com/communication/doctorconfig/DoctorconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communication.doctorconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
