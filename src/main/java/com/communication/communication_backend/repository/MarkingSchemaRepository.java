72e4bcf71d9fa5df00fb82b9ef7e2479633a9caf
Lin Yixiu <melodicoder@users.noreply.github.com>
2025-02-26 01:23:51 +0800

    Edited some stuff

--- src/main/java/com/communication/communication_backend/repository/MarkingSchemaRepository.java
+++ src/main/java/com/communication/communication_backend/repository/MarkingSchemaRepository.java
@@ -1,7 +1,8 @@
 package com.communication.communication_backend.repository;
 
-import com.communication.communication_backend.dtos.MarkingSchema;
-import com.communication.communication_backend.dtos.Scenario;
+import com.communication.communication_backend.entity.MarkingSchema;
+import com.communication.communication_backend.entity.Scenario;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
